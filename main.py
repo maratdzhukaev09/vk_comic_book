733a5e024af5f6c1c22f26e58b2308a1c35727f8
maratdzhukaev09 <60516734+maratdzhukaev09@users.noreply.github.com>
2020-07-09 18:50:32 +0300

    Move .env  variables to ifmain

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import random
 from dotenv import load_dotenv
 
+
 def download_picture(url, filename):
     response = requests.get(url)
     response.raise_for_status()
@@ -10,8 +11,7 @@ def download_picture(url, filename):
     with open(filename, 'wb') as file:
         file.write(response.content)
 
-def get_vk_api_response(api_method, params, requests_method):
-    params["access_token"] = os.getenv("VK_ACCESS_TOKEN")
+
 def get_response(url, params={}):
     response = requests.get(url, params=params)
     response.raise_for_status()
@@ -22,6 +22,10 @@ def get_response(url, params={}):
         raise requests.exceptions.HTTPError(json_data['error'])
     else:
         return response
+
+
+def get_vk_api_response(api_method, params, requests_method, vk_access_token):
+    params["access_token"] = vk_access_token
     params["v"] = "5.120"
     url = f"https://api.vk.com/method/{api_method}/"
 
@@ -49,7 +53,8 @@ def get_last_comic_number():
 
     return picture_comment, picture_filename
 
-def save_photo(picture_filename, server_url):
+
+def save_photo(picture_filename, server_url, vk_access_token, vk_group_id):
     with open(picture_filename, "rb") as file:
         url = server_url
         files = {"photo": file}
@@ -66,29 +71,34 @@ def save_photo(picture_filename, server_url):
         "hash": hash,
         "photo": photo
     }
-    photos_info = get_vk_api_response("photos.saveWallPhoto", params, "POST")
+    photos_info = get_vk_api_response("photos.saveWallPhoto", params, "POST", vk_access_token)
 
     return photos_info
 
-def publish_photo(photos_info, picture_comment):
+
+def publish_photo(photos_info, picture_comment, vk_access_token, vk_group_id):
     params = {
-        "owner_id": f"-{os.getenv('VK_GROUP_ID')}",
+        "owner_id": f"-{vk_group_id}",
         "from_group": 1,
         "message": picture_comment,
         "attachments": f"photo{photos_info['response'][0]['owner_id']}_{photos_info['response'][0]['id']}"
     }
-    return response_json
     json_data = get_vk_api_response("wall.post", params, "POST", vk_access_token)
 
 def main():
     load_dotenv()
     picture_comment, picture_filename = get_comic()
 
-    response_json = get_vk_api_response("photos.getWallUploadServer", {"group_id": os.getenv('VK_GROUP_ID')}, "GET")
     server_url = response_json["response"]["upload_url"]
 
     photos_info = save_photo(picture_filename, server_url)
     publish_photo(photos_info, picture_comment)
+        vk_access_token = os.getenv("VK_ACCESS_TOKEN")
+        vk_group_id = os.getenv("VK_GROUP_ID")
+            {"group_id": vk_group_id},
+            vk_access_token
+        photos_info = save_photo(picture_filename, server_url,  vk_access_token, vk_group_id)
+        publish_photo(photos_info, picture_comment, vk_access_token, vk_group_id)
 
     os.remove(picture_filename)
 
